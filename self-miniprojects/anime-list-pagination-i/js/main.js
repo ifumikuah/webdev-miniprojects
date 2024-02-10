0be00a1499dc9270dc2183e8c309c69cceb45076
ifumikuah <detruxified@proton.me>
2024-02-10 19:38:09 +0700

    modify anime-list-pagination-i

--- self-miniprojects/anime-list-pagination-i/js/main.js
+++ self-miniprojects/anime-list-pagination-i/js/main.js
@@ -1,7 +1,7 @@
 // Import the Bootstrap bundle
 //
 // This includes Popper and all of Bootstrap's JS plugins.
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 //
 // Place any custom JS here
