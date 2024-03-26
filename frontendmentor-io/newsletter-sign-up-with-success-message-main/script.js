774c7f227aebb5c04de5ceb9914b17540af49620
ifumikuah <detruxified@proton.me>
2024-03-27 06:16:49 +0700

    add new post(s)

---
+++ frontendmentor-io/newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,26 @@
+const hero = document.getElementById("hero-img");
+
+hero.src =
+  window.innerWidth <= 475
+    ? "assets/images/illustration-sign-up-mobile.svg"
+    : "assets/images/illustration-sign-up-desktop.svg";
+
+
+const emailForm = document.getElementById("email-form");
+const emailInput = document.getElementById("email-input");
+const formSubmit = document.getElementById("form-submit");
+
+const validateInput = (e) => {
+  e.preventDefault()
+  const checkInput = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi.test(emailInput.value);
+  const isValid = checkInput ? "true" : "false";
+  emailForm.setAttribute("data-valid", isValid)
+
+  if (emailForm.getAttribute("data-valid") === "true") {
+    window.location.replace(
+      `${window.location.origin}/success.html?mail=${emailInput.value}`
+    );  
+  }
+}
+
+formSubmit.addEventListener("click", (e) => validateInput(e));
